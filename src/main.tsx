113a6999a64260d5665dbd4dfb504f75beb8d4e3
Warmor <findwarmor@gmail.com>
2024-10-27 00:40:53 +0200

    som

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import { Provider } from "react-redux";
 import "./index.css";
 import "./App.css";
 import store from "./store/store.ts";
 
 createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-        <Provider store={store}>
             <App />
-        </Provider>
-    </StrictMode>,
 );

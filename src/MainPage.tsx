3e1c35370e8e95866dbf0963221e8bd8298caa5b
Warmor <findwarmor@gmail.com>
2024-10-26 23:39:30 +0200

    som

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -18,7 +18,7 @@ function Header() {
         <header
             className="grid grid-cols-1 place-items-center  relative  justify-center py-20 items-center bg-cover bg-center"
             style={{
-                backgroundImage: "url('whale.jpg')",
+                backgroundImage: "url('/images/bilder/whale.jpg')",
                 backgroundSize: "cover",
                 backgroundRepeat: "no-repeat",
             }}

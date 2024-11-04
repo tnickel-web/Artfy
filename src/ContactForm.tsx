ec710b4ea3b7931b0dd34ec1428d89417937582d
Sawkiii <simon.rohde98@gmail.com>
2024-11-04 15:27:44 +0100

    Update ContactForm.tsx

--- src/ContactForm.tsx
+++ src/ContactForm.tsx
@@ -37,6 +37,7 @@ const ContactForm: React.FC = () => {
         .sendForm(
           import.meta.env.VITE_SERVICE_ID,
           import.meta.env.VITE_TEMPLATE_ID,
+          import.meta.env.VITE_PUBLIC_KEY,
           formRef.current,
           import.meta.env.VITE_OPTIONS
         )

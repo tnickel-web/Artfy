9a979dcb0b19fdaf31b4e8cf86eed7f764bdc5b0
Siro <siro@uni-bremen.de>
2024-11-04 16:10:50 +0100

    wrong key name

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import emailjs from "@emailjs/browser";
 
 function App(): React.ReactElement {
   useEffect(() => {
-    emailjs.init({ publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY });
+    emailjs.init({ publicKey: import.meta.env.VITE_PUBLIC_KEY });
   }, []);
   return <MainPage />;
 }

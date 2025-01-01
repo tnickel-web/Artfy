c16afd92aefa1f1ee92d4351d25aa9fdc69005de
tnickel-web <timonnickel@web.de>
2025-01-01 20:21:54 +0100

    [REFACTOR]: remove file extension from import

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -3,7 +3,7 @@ import ContactForm from "./ContactForm";
 import InstagramFeed from "./InstagramFeed";
 import { Me } from "./Me";
 import Navigation from "./Navigation";
-import Gallery from "./Gallery.tsx";
+import Gallery from "./Gallery";
 import PersonalInformation from "./PersonalInformation";
 
 function App(): React.ReactElement {

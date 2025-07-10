7218be04c0fe859c2fff4da229c0de4e69106d35
tnickel-web <timonnickel@web.de>
2025-07-10 20:17:19 +0200

    [BUGFIX] mobile - fix bottom image cutoff

--- src/Components/Gallery.tsx
+++ src/Components/Gallery.tsx
@@ -3,7 +3,7 @@ import { images, ImageCategory } from "../utils/image-data.ts";
 import type { Image } from "../utils/image-data.ts";
 
 const Gallery = (): React.ReactElement => {
-  const initialCategory = ImageCategory.Other;
+  const initialCategory: string = ImageCategory.Other;
 
   const [selectedImage, setSelectedImage] = useState<Image | null>(null);
   const [expandedCategories, setExpandedCategories] = useState<string[]>([

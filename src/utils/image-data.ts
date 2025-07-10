1b877ba6095fd43da35fd04c4ceffe108874c5d2
tnickel-web <timonnickel@web.de>
2025-07-10 18:57:24 +0200

    [TASK] add missing images

--- src/utils/image-data.ts
+++ src/utils/image-data.ts
@@ -1,14 +1,21 @@
-import whaleImg from "/bilder/whale_crop.jpg";
+import whaleImg from "/bilder/whale.jpg";
 import whale2Img from "/bilder/whale2.jpg";
+import whale3Img from "/bilder/whale3.jpg";
 import rochenImg from "/bilder/rochen.jpg";
 import octopusImg from "/bilder/octopus.jpg";
 import sunflowerImg from "/bilder/sunflower.jpg";
+import sunflower2Img from "/bilder/sunflower2.jpg";
 import unicornImg from "/bilder/unicorn.jpg";
-import womenImg from "/bilder/women.jpg";
+import womanImg from "/bilder/woman.jpg";
 import waldImg from "/bilder/wald.jpg";
 import whiskeyImg from "/bilder/whiskey.jpg";
 import leavesImg from "/bilder/leaves.jpg";
 import spaceImg from "/bilder/space.jpg";
+import kidsRoomImg from "/bilder/kids_room.jpg";
+import featherImg from "/bilder/feather.jpg";
+import gymImg from "/bilder/gym.jpg";
+import tabaklagerImg from "/bilder/tabaklager.jpg";
+import musterImg from "/bilder/muster.jpg";
 
 export interface Image {
   src: string;
@@ -25,56 +32,91 @@ export const images: Image[] = [
   {
     src: leavesImg,
     category: ImageCategory.WallArt,
-    alt: "Wandmalerei Blätter",
+    alt: "Wandmalerei von Blätter",
   },
   {
     src: spaceImg,
     category: ImageCategory.WallArt,
-    alt: "Wandmalerei Weltraum",
+    alt: "Wandmalerei von Weltraum",
   },
   {
     src: whaleImg,
     category: ImageCategory.WallArt,
-    alt: "Wandmalerei Wal",
+    alt: "Wandmalerei von Wal",
   },
   {
     src: whale2Img,
     category: ImageCategory.WallArt,
-    alt: "Wandmalerei Wal 2",
+    alt: "Wandmalerei von Wal 2",
+  },
+  {
+    src: whale3Img,
+    category: ImageCategory.WallArt,
+    alt: "Wandmalerei von Wal 3",
   },
   {
     src: rochenImg,
     category: ImageCategory.WallArt,
-    alt: "Wandmalerei Rochen",
+    alt: "Wandmalerei von Rochen",
   },
   {
     src: octopusImg,
     category: ImageCategory.WallArt,
-    alt: "Wandmalerei Oktopus",
+    alt: "Wandmalerei von Oktopus",
   },
   {
     src: sunflowerImg,
     category: ImageCategory.Other,
-    alt: "Malerei Sonnenblume",
+    alt: "Malerei von Sonnenblume",
+  },
+  {
+    src: sunflower2Img,
+    category: ImageCategory.WallArt,
+    alt: "Wandmalerei von Sonnenblume 2",
   },
   {
     src: unicornImg,
     category: ImageCategory.WallArt,
-    alt: "Wandmalerei Einhorn",
+    alt: "Wandmalerei von Einhorn",
   },
   {
-    src: womenImg,
+    src: womanImg,
     category: ImageCategory.Other,
-    alt: "Malerei Frau",
+    alt: "Malerei von Frau",
   },
   {
     src: waldImg,
     category: ImageCategory.WallArt,
-    alt: "Wandmalerei Wald",
+    alt: "Wandmalerei von Wald",
   },
   {
     src: whiskeyImg,
     category: ImageCategory.WallArt,
-    alt: "Wandmalerei Whiskey",
+    alt: "Wandmalerei von Whiskey",
+  },
+  {
+    src: kidsRoomImg,
+    category: ImageCategory.WallArt,
+    alt: "Wandmalerei in Spielzimmer mit Computertisch",
+  },
+  {
+    src: featherImg,
+    category: ImageCategory.WallArt,
+    alt: "Wandmalerei von Feder",
+  },
+  {
+    src: gymImg,
+    category: ImageCategory.WallArt,
+    alt: "Wandmalerei in Sportstudio",
+  },
+  {
+    src: tabaklagerImg,
+    category: ImageCategory.WallArt,
+    alt: "Wandmalerei in Tabaklager",
+  },
+  {
+    src: musterImg,
+    category: ImageCategory.WallArt,
+    alt: "Wandmalerei (Musterfoto)",
   },
 ];

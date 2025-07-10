f690d09041feb06e477bbfe5921c3fef36c76a78
tnickel-web <timonnickel@web.de>
2025-07-10 17:59:36 +0200

    [REFACTOR] remove useRef usage

--- src/Components/Gallery.tsx
+++ src/Components/Gallery.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import { images, ImageCategory } from "../utils/image-data.ts";
 import type { Image } from "../utils/image-data.ts";
 
@@ -45,7 +45,6 @@ const Gallery = (): React.ReactElement => {
       ).length;
 
       const isExpanded = expandedCategories.includes(category);
-      const contentRef = React.useRef<HTMLDivElement>(null);
 
       return (
         <div key={category} className="mb-8">
@@ -58,14 +57,9 @@ const Gallery = (): React.ReactElement => {
           </h3>
 
           <div
-            ref={contentRef}
-            style={{
-              maxHeight: isExpanded
-                ? `${contentRef.current?.scrollHeight || 0}px`
-                : "0px",
-              transition: "max-height 500ms ease-in-out",
-              overflow: "hidden",
-            }}
+            className={`overflow-hidden transition-all duration-500 ease-in-out ${
+              isExpanded ? "max-h-[2000px] opacity-100" : "max-h-0 opacity-0"
+            }`}
           >
             <div
               className={`columns-1 gap-4 [&>img:not(:first-child)]:mt-8 ${

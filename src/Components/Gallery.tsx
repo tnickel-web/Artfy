ab5d9ae9d37ce6b39e411e3578f9baa3127d8c3e
tnickel-web <timonnickel@web.de>
2025-07-25 22:18:25 +0200

    [TASK] change initially opened category

--- src/Components/Gallery.tsx
+++ src/Components/Gallery.tsx
@@ -3,7 +3,7 @@ import { images, ImageCategory } from "../utils/image-data.ts";
 import type { Image } from "../utils/image-data.ts";
 
 const Gallery = (): React.ReactElement => {
-  const initialCategory: string = ImageCategory.Other;
+  const initialCategory: string = ImageCategory.Kids;
 
   const [selectedImage, setSelectedImage] = useState<Image | null>(null);
   const [expandedCategories, setExpandedCategories] = useState<string[]>([

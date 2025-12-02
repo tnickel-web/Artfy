6c3b0671eb9fa4d780d281bb4631d88b5ba7def8
T. Nickel <63322940+tnickel-web@users.noreply.github.com>
2025-12-02 09:41:32 +0100

    [TASK] Update Email Address (#37)
    * [TASK] update dependencies
    
    * [TASK] update email address
    
    * [TASK] update email address

--- src/utils/personal-information.ts
+++ src/utils/personal-information.ts
@@ -2,7 +2,7 @@ const PersonalInformation = {
   facebook: "https://www.facebook.com/share/1EgfbFocoa/?mibextid=LQQJ4d",
   instagram: "https://www.instagram.com/tamy.luga/",
   telefon: "+49 163 7159501",
-  email: "t.akova@artfy-hb.de",
+  email: "atelier@tamyluga.de",
 } as const;
 
 export default PersonalInformation;

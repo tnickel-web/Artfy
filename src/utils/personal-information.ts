7013eca48ab695528baccbc1a43de2ce10a49d6a
tnickel-web <timonnickel@web.de>
2025-12-02 09:39:54 +0100

    [TASK] update email address

--- src/utils/personal-information.ts
+++ src/utils/personal-information.ts
@@ -2,7 +2,7 @@ const PersonalInformation = {
   facebook: "https://www.facebook.com/share/1EgfbFocoa/?mibextid=LQQJ4d",
   instagram: "https://www.instagram.com/tamy.luga/",
   telefon: "+49 163 7159501",
-  email: "atelier@tamyluga.de"
+  email: "atelier@tamyluga.de",
 } as const;
 
 export default PersonalInformation;

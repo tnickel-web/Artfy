9e73a2ba0528dc91afaefed4b9a58aa5c4f6f569
tnickel-web <timonnickel@web.de>
2026-02-07 20:42:26 +0100

    [TASK] update about me text

--- src/Components/Me.tsx
+++ src/Components/Me.tsx
@@ -13,24 +13,23 @@ export const Me = (): React.ReactElement => {
           <div className="col-span-2">
             <h2 className="text-3xl font-bold mb-4 mt-4">Moin!</h2>
             <p className="text-lg">
-              Hier werden individuelle Wandgestaltungen, kreative Murals und
-              lebendige Fassadengestaltungen mit Farbe - innen sowie außen -
-              umgesetzt. Ob im Kinderzimmer, Café, Büro oder Hallen. Quasi
-              überall, wo Wandflächen stehen. Mit meiner Erfahrung, Ideen und
-              Leidenschaft gestalte ich Wandbilder, die Räume verwandeln, nach
-              deiner Vorstellung oder mit meinem kreativen Konzept.
+              Ich bin Tamara Akova (Tammi) und arbeite seit Ende 2024 als
+              freiberufliche Künstlerin. Ich gestalte individuelle Wandmalereien
+              und arbeite aktuell in Bremen und Umgebung, aber auch darüber
+              hinaus. Meine Arbeiten enstehen direkt auf Wänden und Fassaden,
+              innen sowie außen.
             </p>
           </div>
 
           {/* Zweite Reihe - Text links, Bild rechts */}
           <div className="col-span-1 self-center">
-            <h2 className="text-2xl font-bold mb-4">Über mich</h2>
+            {/*<h2 className="text-2xl font-bold mb-4">Über mich</h2>*/}
             <p className="text-lg">
-              Ich bin Tamara Akova (Tammi), Malermeisterin und freiberufliche
-              Künstlerin. Seit 2024 bin ich selbstständig mit dem was mich
-              begeistert: Wandmalerei, Mural Art und Fassadengestaltung. Ich
-              verbinde mein handwerkliches und künstlerisches Wissen, um
-              Wandflächen kreativ zu gestalten.
+              Meine Ausbildung als Malermeisterin bildet die technische
+              Grundlage für die Umsetzung meiner selbst entwickelten Motiv- und
+              Farbkonzepte. Im Zentrum meiner Arbeit steht jedoch der
+              künstlerische Entwurf und die individuelle Gestaltung des
+              jeweiligen Ortes.
             </p>
           </div>
           <div className="flex justify-center items-center">
@@ -45,11 +44,10 @@ export const Me = (): React.ReactElement => {
           <div className="col-span-2">
             <h2 className="text-2xl font-bold mb-4">Bereit für Farbe?</h2>
             <p className="text-lg mb-4">
-              Ob du schon eine klare Vorstellung hast oder mir einfach freie
-              Hand lassen möchtest. Wir machen einen unverbindlichen Termin aus
-              und besprechen deine Ideen und meine Vorschläge. Die Preise
-              richten sich nach Aufwand, Größe und dem Detailgrad des Projekts.
-              Wenn alles passt, wird es schon bald bunt auf deiner Wand.
+              Wenn du Interesse an einer künstlerischen Wandmalerei hast,
+              tauschen wir uns unverbindlich über Ideen, Raum und Möglichkeiten
+              aus. Auf dieser Basis entwickle ich ein individuelles Konzept und
+              setzte es direkt vor Ort um.
             </p>
           </div>
         </div>
